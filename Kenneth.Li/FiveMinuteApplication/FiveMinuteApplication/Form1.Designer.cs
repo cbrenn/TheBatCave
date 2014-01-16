ef617f9b2a30fc734b94a7df0d68b95b12a8b3aa
kenli6 <kenli6@remove.gmail.remove.com>
2014-01-16 15:42:56 -0800

    Version 2 - Testing Sound

--- Kenneth.Li/FiveMinuteApplication/FiveMinuteApplication/Form1.Designer.cs
+++ Kenneth.Li/FiveMinuteApplication/FiveMinuteApplication/Form1.Designer.cs
@@ -29,65 +29,122 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
-            this.huskyPictureBox = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.huskyTab = new System.Windows.Forms.TabPage();
+            this.stopHuskySong = new System.Windows.Forms.Button();
             this.huskyGradPictureBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.huskyPictureBox)).BeginInit();
+            this.huskyPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.huskyTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.huskyGradPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.huskyPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // tabControl1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(158, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "This is the UW Husky, click me!";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.tabControl1.Controls.Add(this.huskyTab);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(16, 11);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(197, 411);
+            this.tabControl1.TabIndex = 0;
             // 
-            // huskyPictureBox
+            // huskyTab
             // 
-            this.huskyPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("huskyPictureBox.Image")));
-            this.huskyPictureBox.Location = new System.Drawing.Point(50, 44);
-            this.huskyPictureBox.Name = "huskyPictureBox";
-            this.huskyPictureBox.Size = new System.Drawing.Size(99, 95);
-            this.huskyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.huskyPictureBox.TabIndex = 1;
-            this.huskyPictureBox.TabStop = false;
+            this.huskyTab.Controls.Add(this.stopHuskySong);
+            this.huskyTab.Controls.Add(this.huskyGradPictureBox);
+            this.huskyTab.Controls.Add(this.huskyPictureBox);
+            this.huskyTab.Controls.Add(this.label1);
+            this.huskyTab.Location = new System.Drawing.Point(4, 22);
+            this.huskyTab.Name = "huskyTab";
+            this.huskyTab.Padding = new System.Windows.Forms.Padding(3);
+            this.huskyTab.Size = new System.Drawing.Size(189, 385);
+            this.huskyTab.TabIndex = 0;
+            this.huskyTab.Text = "Click if you\'re a Husky";
+            this.huskyTab.UseVisualStyleBackColor = true;
+            // 
+            // stopHuskySong
+            // 
+            this.stopHuskySong.Location = new System.Drawing.Point(25, 297);
+            this.stopHuskySong.Name = "stopHuskySong";
+            this.stopHuskySong.Size = new System.Drawing.Size(115, 50);
+            this.stopHuskySong.TabIndex = 6;
+            this.stopHuskySong.Text = "Stop Playing Song";
+            this.stopHuskySong.UseVisualStyleBackColor = true;
+            this.stopHuskySong.Click += new System.EventHandler(this.stopHuskySong_Click);
             // 
             // huskyGradPictureBox
             // 
             this.huskyGradPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("huskyGradPictureBox.Image")));
-            this.huskyGradPictureBox.Location = new System.Drawing.Point(22, 35);
+            this.huskyGradPictureBox.Location = new System.Drawing.Point(6, 29);
             this.huskyGradPictureBox.Name = "huskyGradPictureBox";
-            this.huskyGradPictureBox.Size = new System.Drawing.Size(148, 224);
-            this.huskyGradPictureBox.TabIndex = 2;
+            this.huskyGradPictureBox.Size = new System.Drawing.Size(150, 225);
+            this.huskyGradPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.huskyGradPictureBox.TabIndex = 5;
             this.huskyGradPictureBox.TabStop = false;
+            // 
+            // huskyPictureBox
+            // 
+            this.huskyPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("huskyPictureBox.Image")));
+            this.huskyPictureBox.Location = new System.Drawing.Point(25, 52);
+            this.huskyPictureBox.Name = "huskyPictureBox";
+            this.huskyPictureBox.Size = new System.Drawing.Size(99, 95);
+            this.huskyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.huskyPictureBox.TabIndex = 4;
+            this.huskyPictureBox.TabStop = false;
+            this.huskyPictureBox.Click += new System.EventHandler(this.huskyPictureBox_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hi fellow Husky, please click on me!";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(277, 435);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(196, 323);
-            this.Controls.Add(this.huskyGradPictureBox);
-            this.Controls.Add(this.huskyPictureBox);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(225, 452);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Five Minute Program";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.huskyPictureBox)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.huskyTab.ResumeLayout(false);
+            this.huskyTab.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.huskyGradPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.huskyPictureBox)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox huskyPictureBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage huskyTab;
         private System.Windows.Forms.PictureBox huskyGradPictureBox;
+        private System.Windows.Forms.PictureBox huskyPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button stopHuskySong;
+
     }
 }
 
--- Kenneth.Li/FiveMinuteApplication/FiveMinuteApplication/Form1.cs
+++ Kenneth.Li/FiveMinuteApplication/FiveMinuteApplication/Form1.cs
@@ -7,15 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace FiveMinuteApplication
 {
     public partial class Form1 : Form
     {
+        SoundPlayer player = new SoundPlayer();
         public Form1()
         {
             InitializeComponent();
             huskyGradPictureBox.Hide();
+            stopHuskySong.Hide();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -23,15 +26,31 @@ namespace FiveMinuteApplication
 
         }
 
-        private void huskyPictureBox_Click(object sender, EventArgs e)
-        {
-            huskyGradPictureBox.Show();
-            label1.Hide();
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void huskyPictureBox_Click_1(object sender, EventArgs e)
+        {
+            huskyGradPictureBox.Show();
+            label1.Hide();
+            player = new
+            SoundPlayer(FiveMinuteApplication.Properties.Resources.Baha_Men___Who_Let_The_Dogs_Out);
+            player.Play();
+            stopHuskySong.Show();
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void stopHuskySong_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
     }
 }
