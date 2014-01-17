8c0893321773342b69058f8e1c22eb78cb0ea0f1
Alex Aragon <aragonwa@gmail.com>
2014-01-17 10:00:13 -0800

    Updating Assignment 1
    Added some buttons, logic.

--- Alex.Aragon/Assignment1/Assignment1/Form1.cs
+++ Alex.Aragon/Assignment1/Assignment1/Form1.cs
@@ -16,5 +16,28 @@ namespace Assignment1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult feedback1 = MessageBox.Show("Wasn't that fun?",
+            "You clicked it!",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question);
+
+            if (feedback1 == DialogResult.Yes)
+            {
+                MessageBox.Show("If was fun for me too");
+            }
+            else {
+                MessageBox.Show("Give me a break it's my first C# program.");
+            }
+
+
+        }
     }
 }
