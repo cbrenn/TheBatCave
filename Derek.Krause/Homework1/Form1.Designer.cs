73a9f246d145b5a81960528720912de5a1f58347
derekkrause <krause96@gmail.com>
2014-01-19 11:16:34 -0800

    Changes to "Two Buttons Better Than One"
    New and improved!

--- Derek.Krause/Homework1/Form1.Designer.cs
+++ Derek.Krause/Homework1/Form1.Designer.cs
@@ -28,15 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(307, 35);
+            this.button1.Size = new System.Drawing.Size(536, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Click Me";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,19 +50,35 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 171);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(12, 375);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(307, 36);
+            this.button2.Size = new System.Drawing.Size(536, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "Not Me";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 316);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 301);
+            this.ClientSize = new System.Drawing.Size(560, 423);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -66,6 +88,7 @@
             this.Text = "Excited to Learn More";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseEnter += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -74,6 +97,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Derek.Krause/Homework1/Form1.cs
+++ Derek.Krause/Homework1/Form1.cs
@@ -19,7 +19,7 @@ namespace Homework1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,6 +30,31 @@ namespace Homework1
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Uh, oh!");
+            MessageBox.Show("Now you're in for it!");
+            MessageBox.Show("You should have listened...");
+            MessageBox.Show("5");
+            MessageBox.Show("4");
+            MessageBox.Show("3");
+            MessageBox.Show("2 and a half");
+            MessageBox.Show("2 and a quarter...");
+            MessageBox.Show("ONE!");
+            MessageBox.Show("");
+            MessageBox.Show("                      ");
+            MessageBox.Show("Is everything still alright?");
+            MessageBox.Show("Good");
+            MessageBox.Show("Now, let's sing a song together, shall we?");
+            MessageBox.Show("What's your favorite song?");
+            MessageBox.Show("I bet you can't guess mine...");
+            MessageBox.Show("99 bottles of beer on the wall");
+            MessageBox.Show("99 bottles of BEER!");
+            MessageBox.Show("Take one down, pass it around...");
+            MessageBox.Show("Come on, you're not singing along!");
+            MessageBox.Show("You're no fun.");
+            MessageBox.Show("Well, see ya later!");
+
         }
     }
 }
+
+
+
