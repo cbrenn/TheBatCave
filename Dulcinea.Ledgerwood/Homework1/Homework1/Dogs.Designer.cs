0badb9f6a0d17fc3d7d739ff87a254d9031be8be
dulcinealedgerwood <dulcinealedgerwood>
2014-01-19 17:15:13 -0800

    Updating homework with puppies
    It's the best I got right now.  go hawks!

---
+++ Dulcinea.Ledgerwood/Homework1/Homework1/Dogs.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Homework1
+{
+    partial class Dogs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.moarBtn = new System.Windows.Forms.Button();
+            this.dogsPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dogsPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // moarBtn
+            // 
+            this.moarBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.moarBtn.Location = new System.Drawing.Point(921, 79);
+            this.moarBtn.Name = "moarBtn";
+            this.moarBtn.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.moarBtn.Size = new System.Drawing.Size(127, 62);
+            this.moarBtn.TabIndex = 1;
+            this.moarBtn.Text = "Show me moar!";
+            this.moarBtn.UseVisualStyleBackColor = true;
+            this.moarBtn.Click += new System.EventHandler(this.moarBtn_Click);
+            // 
+            // dogsPB
+            // 
+            this.dogsPB.Image = global::Homework1.Properties.Resources.NayaBaileyOreo;
+            this.dogsPB.Location = new System.Drawing.Point(198, 79);
+            this.dogsPB.Name = "dogsPB";
+            this.dogsPB.Size = new System.Drawing.Size(604, 407);
+            this.dogsPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.dogsPB.TabIndex = 2;
+            this.dogsPB.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1106, 653);
+            this.Controls.Add(this.dogsPB);
+            this.Controls.Add(this.moarBtn);
+            this.Name = "Form1";
+            this.Text = "I love my puppies";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dogsPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button moarBtn;
+        private System.Windows.Forms.PictureBox dogsPB;
+    }
+}
+
---
+++ Dulcinea.Ledgerwood/Homework1/Homework1/Dogs.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using Homework1.Properties;
+
+namespace Homework1
+{
+    public partial class Dogs : Form
+    {
+        readonly Image[] dogImages = new Image[6];
+       
+        public Dogs()
+        {
+            InitializeComponent();
+            
+            // load image array - puppies!
+            dogImages[0] = Resources.NayaBaileyOreo;
+            dogImages[1] = Resources.Bailey2;
+            dogImages[2] = Resources.Bert10;
+            dogImages[3] = Resources.BertZeke;
+            dogImages[4] = Resources.BertPool2;
+            dogImages[5] = Resources.Gurgle;
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moarBtn_Click(object sender, EventArgs e)
+        {
+            // hide our first picture box and make the backcolor dark gray.
+            dogsPB.Hide();
+            this.BackColor = Color.DarkSlateGray;
+
+            // We'll collage these picture boxes on the form.
+            PictureBox[] picturebox= new PictureBox[dogImages.Length-1];
+
+            for (int index = 0; index < picturebox.Length; index++)
+            {
+                picturebox[index] = new PictureBox();
+                this.Controls.Add(picturebox[index]);
+
+                // I tried to determine the axis points dynamically but gave up after a failed try.
+                switch (index)
+                {
+                    case 0:
+                        picturebox[index].Location = new Point(20, 20);
+                        break;
+                    case 1:
+                        picturebox[index].Location = new Point(20, 250);
+                        break;
+                    case 2:
+                        picturebox[index].Location = new Point(250, 20);
+                        break;
+                    case 3:
+                        picturebox[index].Location = new Point(200, 220);
+                        break;
+                    case 4:
+                        picturebox[index].Location = new Point(500, 20);
+                        break;
+                }
+
+                picturebox[index].Size = new Size(dogImages[index+1].Height, dogImages[index+1].Width);
+                picturebox[index].SizeMode = PictureBoxSizeMode.AutoSize;
+                picturebox[index].Image = dogImages[index+1];
+                
+            }
+
+            // For now, hide the button.  If we have time we'll look into making it return to
+            // to the previous picture.
+            moarBtn.Hide();
+
+        }
+    }
+}
